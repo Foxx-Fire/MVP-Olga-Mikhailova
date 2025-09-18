16a082f7eb53caf7d4eee31ddf4c6047353cdd6c
Olga Mikhailova <foxx_fire@mail.ru>
2025-09-18 21:41:34 +0300

    Initial Commit

---
+++ MVP-Olga Mikhailova/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVP-Olga Mikhailova
+//
+//  Created by FoxxFire on 18.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
